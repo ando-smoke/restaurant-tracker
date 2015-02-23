f77cb422b2c18acc626e80a22079f6ae12d46f99
Jeyananthan Sivakumaran <jsivakumaran@outlook.com>
2015-02-23 11:18:50 -0800

    Add dynamic population of new restaurant form dropdown menus.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,6 +1,12 @@
 restaurantsTracker.controller("RestaurantsCtrl",
   function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
+  $scope.foodTypes= ["Chinese", "Italian", "French", "Indian", "Mexican",
+    "Steak and Seafood"];
+  $scope.locations = ["Downtown", "Northeast PDX", "Northwest PDX",
+    "Pearl District", "Southeast PDX", "Southwest PDX"];
+  $scope.priceRanges = ["$", "$$", "$$$"];
+  
   $scope.addRestaurant = function() {
     $scope.restaurants.push({
       name: $scope.restaurantName,
